bce2b9f5146de6f070900081c79f7dc3e645fe72
Sebastian Aedo <aedovis@gmatc.matc.edu>
2025-09-10 16:36:31 -0500

    Completed Activity 3

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba86dc75742e8333dbf5591fd995fd74fb620545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bbf149055ca0c96a0c071e9921d2cc8f89dc77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
